0a1cf0e026137091be29d1d3c3d11ad0ce56ec1c
Pijush Chakraborty <piyushchakraborty9@gmail.com>
2019-09-16 13:03:07 +0530

    array exercise completes

--- array-methods/2-array.js
+++ array-methods/2-array.js
@@ -9,14 +9,34 @@ var words = [
 ];
 //Write a function findLongestWord that takes an array of words and returns the longest one.
 //If there are 2 with the same length, it should return the first occurrence.
+var count = "";
+function findLongestWord(words) {
+  words.forEach(item => {
+    if (item.length > count.length) {
+      count = item;
 
-
-
+    }
+  })
+  console.log(count)
+}
+findLongestWord(words)
 
 var numbers1 = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 // Create a sumArray function that takes an array of numbers1 as a parameter, and calculate the sum of all its numbers
 // Use reduce method of array
 // Use the above sum and calculate the average.
+let sum = []
+function sumArray(numbers1) {
+  let a = numbers1.reduce((acc, cv) => {
+    return acc += cv
+
+
+  }, 0)
+  return a / numbers1.length
+
+}
+sumArray(numbers1)
+
 
 
 
@@ -24,8 +44,20 @@ var numbers1 = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 var numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 //Write a function averageNumbers that receives an array of numbers2 and calculate the average of the numbers
 
+// var numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
+// //Write a function averageNumbers that receives an array of numbers2 and calculate the average of the numbers
+function average() {
+  let b = numbers2.length;
+  console.log(b)
+  let a = numbers2.reduce((acc, cv) => {
+    acc += cv
+    
+  }, 0)
+  return acc / b
+  console.log(a)
+}
 
-
+average()
 
 var words2 = [
   'seat',
--- array-methods/4-array.js
+++ array-methods/4-array.js
@@ -1,36 +1,47 @@
-var data = [
-  {
-    name: "Butters",
-    age: 3,
-    type: "dog"
-  },
-  {
-    name: "Lizzy",
-    age: 6,
-    type: "dog"
-  },
-  {
-    name: "Red",
-    age: 1,
-    type: "cat"
-  },
-  {
-    name: "Joey",
-    age: 3,
-    type: "dog"
-  },
-  {
-    name: "Pochi",
-    age: 3,
-    type: "dog"
-  }
-];
+  var data = [
+    {
+      name: "Butters",
+      age: 3,
+      type: "dog"
+    },
+    {
+      name: "Lizzy",
+      age: 6,
+      type: "dog"
+    },
+    {
+      name: "Red",
+      age: 1,
+      type: "cat"
+    },
+    {
+      name: "Joey",
+      age: 3,
+      type: "dog"
+    },
+    {
+      name: "Pochi",
+      age: 3,
+      type: "dog"
+    }
+  ];
 
 // write a function that will sum all of the dogs ages in dog years using for loop.
 // 1 human year = 7 dog year
 
 // your code goes here
+data.forEach(item => {
+  //   console.log(item.type)
+  if (item.type == "dog") {
+    //     console.log (item.age * 7);
+    a = item.age * 7
+    //     console.log( a)
+    b += a
+  }
 
+
+})
+console.log(b)
 // Solution is 105
 
 // Write the same function using
