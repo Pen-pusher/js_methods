64928cac4f64dddc92b89841131e703254828aa4
Pijush Chakraborty <piyushchakraborty9@gmail.com>
2019-09-16 13:59:53 +0530

    object.js completed

--- array-methods/5-object.js
+++ array-methods/5-object.js
@@ -1,6 +1,6 @@
 // 1. Write a JavaScript program to list the properties and values of a JavaScript object. (Object.keys)
 
-
+  
 
 // 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
 var student = { 
@@ -9,7 +9,22 @@ var student = {
   rollno : 12
 };
 
-
+function student_properties() {
+  console.log(student)
+  console.log(String(student).length)
+  console.log(delete student.rollno)
+  console.log(student)
+}
+student_properties()
 
 
 // 3. Write a function to get the length of an object.
+function student_properties() {
+  console.log(student)
+  console.log(String(student).length)
+  console.log(delete student.rollno)
+  console.log(student)
+}
+student_properties()
+
+// i am consoling the total length of object students.
